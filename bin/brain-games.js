f98fb828c4fbe4da2c1e0acb3fdf877dae4f7cbb
Sergey Kachelya <sergey.kachelya@icloud.com>
2021-08-14 13:45:48 +0200

    изменение шага3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import { meetAndGreet } from '../src/cli.js';
+import welcome from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!\n');
-meetAndGreet();
+welcome();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const meetAndGreet = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+const welcome = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default welcome;
