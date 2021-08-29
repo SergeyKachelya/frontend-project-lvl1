096715a9bac10f5ee2a0433ecc168f5fe3201f46
Sergey Kachelya <sergey.kachelya@icloud.com>
2021-08-29 15:23:53 +0200

    шаг 9

---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import { getRandomNumber } from "../utils.js";
+import engine from "../engine.js";
+const description = "Answer 'Yes' if given number is prime. Otherwise answer 'no'.";
+const isPrime = (elem) => {
+    if (elem <= 1) return false;
+    for (let i = 2; i < Math.sqrt(elem); i += 1) {
+        if (elem % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+const getRoundData = () => {
+    const randomNum = getRandomNumber(0, 100);
+    const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+    const question = randomNum;
+    return { correctAnswer, question };
+};
+const startGame = () => engine(description, getRoundData);
+export default startGame;
