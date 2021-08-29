acd11150008ef1dd654654b91fc925eb1d165cc7
Sergey Kachelya <sergey.kachelya@icloud.com>
2021-08-29 15:38:54 +0200

    linter fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,22 +1,23 @@
-import { getRandomNumber } from "../utils.js";
-import engine from "../engine.js";
-const description = "What number is missing in the progression?";
+import { getRandomNumber } from '../utils.js';
+import engine from '../engine.js';
+
+const description = 'What number is missing in the progression?';
 const getProgression = (start, step, count) => {
-    const arithmeticProgress = [];
-    for (let i = 0; i < count; i += 1) {
-        arithmeticProgress.push(start + step * i);
-        }
-        return arithmeticProgress;
-}
+  const arithmeticProgress = [];
+  for (let i = 0; i < count; i += 1) {
+    arithmeticProgress.push(start + step * i);
+  }
+  return arithmeticProgress;
+};
 const getRoundData = () => {
-    const randomNum = getRandomNumber(0, 10);
-    const step = getRandomNumber(0, 10);
-    const count = getRandomNumber(5, 10);
-    const randomPosition = getRandomNumber(0, count - 1);
-    const progression = getProgression(randomNum, step, count);
-    const correctAnswer = progression[randomPosition].toString();
-    progression[randomPosition] = '..';
-    return { correctAnswer, question: progression.join(' ')};
+  const randomNum = getRandomNumber(0, 10);
+  const step = getRandomNumber(0, 10);
+  const count = getRandomNumber(5, 10);
+  const randomPosition = getRandomNumber(0, count - 1);
+  const progression = getProgression(randomNum, step, count);
+  const correctAnswer = progression[randomPosition].toString();
+  progression[randomPosition] = '..';
+  return { correctAnswer, question: progression.join(' ') };
 };
 const startGame = () => engine(description, getRoundData);
 export default startGame;
